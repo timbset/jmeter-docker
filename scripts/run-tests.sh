df6130d90dc5f216d7566a297a8e23c852067aed
timbset <timbset@gmail.com>
2022-05-13 14:45:57 +0300

    Implement downloading config

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -3,6 +3,7 @@
 EXECUTION_ID=$(echo $AWS_EVENT | jq .executionId | awk -F ":" '{print $8}' | tr -d '"')
 RESULTS_S3_PREFIX=$(echo $AWS_EVENT | jq .resultsS3Prefix | tr -d '"')
 TEST_PLAN_S3_URI=$(echo $AWS_EVENT | jq .testPlanS3Uri | tr -d '"')
+TEST_CONFIGS_S3_URI=$(echo $AWS_EVENT | jq .testConfigsS3Uri | tr -d '"')
 JMETER_EXTRA_PARAMS=$(echo $AWS_EVENT | jq .jmeterParams | tr -d '"')
 
 if [[ ! -z "$AWS_ACCESS_KEY_ID" ]] && [[ ! -z "$AWS_SECRET_ACCESS_KEY" ]]; then
@@ -18,6 +19,13 @@ if [[ ! -z "$USE_AWS" ]]; then
     && aws s3 cp "${TEST_PLAN_S3_URI}" test-plans/test-plan.jmx \
     && echo "Test plan downloaded" \
     || echo "Failed to download plan"
+fi
+
+if [[ ! -z "$USE_AWS" ]] && [[ ! -z "$TEST_CONFIGS_S3_URI" ]]; then
+  echo "Downloading test configs from ${TEST_CONFIGS_S3_URI}" \
+    && aws s3 cp "${TEST_CONFIGS_S3_URI}" test-plans/configs --recursive \
+    && echo "Test configs downloaded" \
+    || echo "Failed to download configs"
 fi
 
 /entrypoint.sh -n -t test-plans/test-plan.jmx -l test-plans/test-plan.jtl -j test-plans/jmeter.lo -e ${JMETER_EXTRA_PARAMS}
